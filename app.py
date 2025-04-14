fd3d2d9720544ae283cefdbc62228cc4b5ce30fa
Boris0318 <borisesgal@gmail.com>
2025-04-14 13:01:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-import sys
-try:
-    __import__("pysqlite3")
-    sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
-except ImportError:
-    pass
+# import sys
+# try:
+#     __import__("pysqlite3")
+#     sys.modules["sqlite3"] = sys.modules.pop("pysqlite3")
+# except ImportError:
+    # pass
 import os
 import streamlit as st
 import asyncio
